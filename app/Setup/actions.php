6e53fcd10276e2df131eefe6a16fa2e86b23507d
Sean Beck <spcbeck@gmail.com>
2018-12-09 21:31:35 -0500

    Removing stuff from admin menu.

--- app/Setup/actions.php
+++ app/Setup/actions.php
@@ -23,3 +23,22 @@ function example_action()
     //
 }
 add_filter('excerpt_length', 'Pixability\Theme\App\Setup\example_action');
+
+if ( current_user_can( 'adformat_editor' ) ) {
+	function remove_menus(){
+	  
+	  remove_menu_page( 'index.php' );                  //Dashboard
+	  remove_menu_page( 'jetpack' );                    //Jetpack* 
+	  remove_menu_page( 'edit.php' );                   //Posts
+	  remove_menu_page( 'upload.php' );                 //Media
+	  remove_menu_page( 'edit.php?post_type=page' );    //Pages
+	  remove_menu_page( 'edit-comments.php' );          //Comments
+	  remove_menu_page( 'themes.php' );                 //Appearance
+	  remove_menu_page( 'plugins.php' );                //Plugins
+	  remove_menu_page( 'users.php' );                  //Users
+	  remove_menu_page( 'tools.php' );                  //Tools
+	  remove_menu_page( 'options-general.php' );        //Settings
+	  
+	}
+	add_action( 'admin_menu', 'remove_menus' );
+}
