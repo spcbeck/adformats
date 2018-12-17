34e71aa267878f0e4f25309e9a8b62e93509e288
Sean Beck <spcbeck@gmail.com>
2018-12-16 21:35:15 -0500

    Adding flippable cards to homepage.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,5 @@
 import $ from 'jquery'
 
 $('#loginform').prepend('<h2 class="h5">Login Portal</h2><p class="h3">Browse Pixability\'s available video ad inventory and view specifications and requirements.</p>');
+
+document.querySelector(".card-flip").classList.toggle("flip");
--- resources/templates/front.tpl.php
+++ resources/templates/front.tpl.php
@@ -9,19 +9,12 @@
     }
 ?>
 
-<style>
-    .hr-color rect {
-        fill: <?php echo $hrColor ?>;
-    }
-</style>
-
 <section id="hero" class="hero <?php if(has_post_thumbnail()): ?>jarallax<?php endif; ?>" <?php if(has_post_thumbnail()): ?> style="background-image: url('<?php the_post_thumbnail_url( 'large' ); ?>');" <?php endif; ?>>
     <div class="container">
         <div class="row align-items-center">
             <div class="col-md-5 text-white">
                 <h1 style="color: <?php echo $textColor; ?>"><?php the_field('title'); ?></h1>
                 <h2><?php the_field('subtitle'); ?></h2>
-                <svg class="svg hr hr-color" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 67.68 10.41"><defs><style>.cls-1{fill:$hrColor;}</style></defs><title>hero-hr</title><rect class="cls-1" width="2.97" height="2.97"/><rect class="cls-1" x="8.09" width="2.97" height="2.97"/><rect class="cls-1" x="16.18" width="2.97" height="2.97"/><rect class="cls-1" x="24.26" width="2.97" height="2.97"/><rect class="cls-1" x="32.35" width="2.97" height="2.97"/><rect class="cls-1" x="40.44" width="2.97" height="2.97"/><rect class="cls-1" x="48.53" width="2.97" height="2.97"/><rect class="cls-1" x="56.62" width="2.97" height="2.97"/><rect class="cls-1" x="64.7" width="2.97" height="2.97"/><rect class="cls-1" y="7.44" width="2.97" height="2.97"/><rect class="cls-1" x="8.09" y="7.44" width="2.97" height="2.97"/><rect class="cls-1" x="16.18" y="7.44" width="2.97" height="2.97"/><rect class="cls-1" x="24.26" y="7.44" width="2.97" height="2.97"/><rect class="cls-1" x="32.35" y="7.44" width="2.97" height="2.97"/><rect class="cls-1" x="40.44" y="7.44" width="2.97" height="2.97"/><rect class="cls-1" x="48.53" y="7.44" width="2.97" height="2.97"/><rect class="cls-1" x="56.62" y="7.44" width="2.97" height="2.97"/><rect class="cls-1" x="64.7" y="7.44" width="2.97" height="2.97"/></svg>
                 <p><?php the_field('paragraph_text'); ?></p>
                 <div class="d-flex hero-buttons">
                     <?php if( have_rows('hero_buttons') ): ?>
@@ -42,22 +35,56 @@
 </section>
 
 <section class="section">
-    <div class="wrapper">
-        <div class="content">
-            <?php if (have_posts()) : ?>
-                <?php while (have_posts()) : the_post() ?>
-                    <?php
-                        /**
-                         * Functions hooked into `theme/single/content` action.
-                         *
-                         * @hooked Pixability\Theme\App\Structure\render_post_content - 10
-                         */
-                        do_action('theme/single/content');
-                    ?>
+    <div class="container">
+        <div class="row">
+            <?php
+                $args = array(
+                  'post_type' => 'adformat',
+                  'category_name' => 'facebook'
+                );
+                $loop = new WP_Query( $args );
+
+                while ( $loop->have_posts() ) : $loop->the_post();?>
+                    <div class="col-md-4">
+                        <div class="card-flip">
+                            <div class="flip">
+                                <div class="front">
+                                    <div class="card adformat">
+                                        <div class="card-body text-center">
+                                            <h3><?php the_title(); ?></h3>
+                                            <hr>
+                                            <p>
+                                                <?php the_excerpt() ?>
+                                            </p>
+                                        </div>
+                                        <?php
+                                            if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                                                the_post_thumbnail( 'full', array( 'class'  => 'card-img-bottom' ) ); // show featured image
+                                            } 
+                                        ?>
+                                    </div>
+                                </div>
+                                <div class="back">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <h3><?php the_title(); ?></h3>
+                                            <hr>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 <?php endwhile; ?>
-            <?php endif; ?>
-
-
+                <?php wp_reset_query(); ?>
+        </div>
+        <div class="row">
+            <div class="col">
+                <?php if (have_posts()) : ?>
+                    <?php while (have_posts()) : the_post() ?>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
 </section>
