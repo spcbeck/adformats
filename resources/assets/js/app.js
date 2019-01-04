deb5306950a6ef60e3b9fe9e66d997ee69ae20d4
Sean Beck <spcbeck@gmail.com>
2019-01-03 22:43:19 -0500

    Setting height of cards.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -22,16 +22,6 @@ $('#loginform').prepend('<h2 class="h5">Login Portal</h2><p class="h3">Browse Pi
 document.querySelector(".card-flip").classList.toggle("flip");
 
 $(document).ready(function() {
-  $('.flip').each(function() {
-    console.log("okay");
-    var frontHeight = $(this).find('.front').height();
-    var backHeight = $(this).find('.back').height();
-
-    if(frontHeight >= backHeight)
-      $(this).height(frontHeight);
-    else 
-      $(this).height(backHeight);
-  })
 
   $('.card-flip').click(function() {
     $(this).toggleClass('hover');
